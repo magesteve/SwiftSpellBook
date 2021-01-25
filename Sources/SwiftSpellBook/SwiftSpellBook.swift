489050ad539141935faf34c25da1991e0571c44d
Steve Sheets <magesteve@mac.com>
2021-01-25 00:59:28 -0500

    updarte

--- Sources/SwiftSpellBook/SwiftSpellBook.swift
+++ Sources/SwiftSpellBook/SwiftSpellBook.swift
@@ -16,6 +16,13 @@ public struct SwiftSpellBook {
 
 public extension SwiftSpellBook {
     
+    // MARK: Result Closures
+    
+    /// Closure that has no results and no parameters.
+    typealias SimpleClosure = () -> Void
+
+    // MARK: Informative Closures
+    
     /// Closure that has no results, but it passed a URL.
     typealias URLClosure = (URL) -> Void
     
@@ -28,6 +35,21 @@ public extension SwiftSpellBook {
     /// Closure that has no results, but it passed a Int.
     typealias IntClosure = (Int) -> Void
     
+    // MARK: Result Closures
+    
+    /// Closure that has no parameters, but it returns a URL.
+    typealias URLResultClosure = () -> URL
+    
+    /// Closure that has no parameters, but it returns a Data.
+    typealias DataResultClosure = () -> Data
+    
+    /// Closure that has no parameters, but it returns a String.
+    typealias StringResultClosure = () -> String
+    
+    /// Closure that has no parameters, but it returns a Int.
+    typealias IntResultClosure = () -> Int
+    
+
 }
 
 // MARK: String Extensions
