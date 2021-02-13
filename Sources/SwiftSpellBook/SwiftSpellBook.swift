6faddcb5e22d0f61b8cbb77ac3fb7700d12a2219
Steve Sheets <magesteve@mac.com>
2021-02-12 21:56:38 -0500

    update

--- Sources/SwiftSpellBook/SwiftSpellBook.swift
+++ Sources/SwiftSpellBook/SwiftSpellBook.swift
@@ -63,3 +63,96 @@ public extension SwiftSpellBook {
 
 }
 
+// MARK: UserDefaults
+
+// Extension for User Defaults
+public extension SwiftSpellBook {
+    
+    /// Retrieve the User Default string value for given key. If it does not exists, save the default value, and return it.
+    /// - Parameters:
+    ///   - key: String key for value
+    ///   - defaultString: String
+    /// - Returns: String Retrieved Value
+    static func getUserDefault(key: String, defaultString: String) -> String {
+        let ud = UserDefaults.standard
+        if let value = ud.string(forKey: key) {
+            return value
+        }
+        else {
+            ud.set(defaultString, forKey: key)
+            
+            return defaultString
+       }
+    }
+    
+    /// Retrieve the User Default Int value for given key. If it does not exists, save the default value, and return it.
+    /// - Parameters:
+    ///   - key: String key for value
+    ///   - defaultInt: Int
+    /// - Returns: Int Retrieved Value
+    static func getUserDefault(key: String, defaultInt: Int) -> Int {
+        let ud = UserDefaults.standard
+        if let _ = ud.object(forKey: key) {
+            return ud.integer(forKey: key)
+        }
+        else {
+            ud.set(defaultInt, forKey: key)
+            
+            return defaultInt
+       }
+    }
+    
+    /// Retrieve the User Default Bool value for given key. If it does not exists, save the default value, and return it.
+    /// - Parameters:
+    ///   - key: String key for value
+    ///   - defaultBool: Bool
+    /// - Returns: Bool Retrieved Value
+    static func getUserDefault(key: String, defaultBool: Bool) -> Bool {
+        let ud = UserDefaults.standard
+        if let _ = ud.object(forKey: key) {
+            return ud.bool(forKey: key)
+        }
+        else {
+            ud.set(defaultBool, forKey: key)
+            
+            return defaultBool
+       }
+    }
+    
+    /// Set the String Value to the Key
+    /// - Parameters:
+    ///   - key: String key for value
+    ///   - value: String value to save
+    static func setUserDefault(key: String, _ value: String) {
+        let ud = UserDefaults.standard
+        let s = value as NSString
+        ud.set(s, forKey: key)
+    }
+    
+    /// Set the Int Value to the Key
+    /// - Parameters:
+    ///   - key: String key for value
+    ///   - value: Int value to save
+    static func setUserDefault(key: String, _ value: Int) {
+        let ud = UserDefaults.standard
+        ud.set(value, forKey: key)
+    }
+    
+    /// Set the Bool Value to the Key
+    /// - Parameters:
+    ///   - key: String key for value
+    ///   - value: Bool value to save
+    static func setUserDefault(key: String, _ value: Bool) {
+        let ud = UserDefaults.standard
+        ud.set(value, forKey: key)
+    }
+    
+    /// Remove list of keys from User Defaults
+    /// - Parameter keys: Array of String to remove
+    static func removeListUserDefault(keys: [String]) {
+        let ud = UserDefaults.standard
+        for s in keys {
+            ud.removeObject(forKey: s)
+        }
+    }
+}
